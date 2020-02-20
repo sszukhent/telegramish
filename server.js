d7eebd3e00c504a0dfc640a3f2c22a8afe912639
sszukhent <commandershepard989@gmail.com>
2020-02-20 14:05:24 -0500

    directory and server file changes to host app

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const path = require('path');
 
 users = [];
 connections = [];
@@ -10,6 +11,4 @@ server.listen(process.env.PORT || 3000);
 
 console.log('Server running...');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/design/index.html');
-});
+app.use(express.static(path.join(__dirname, 'public')));
