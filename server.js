ecdcaa50e919ef755192b5d7f1b006d57258308c
sszukhent <commandershepard989@gmail.com>
2020-02-20 16:52:18 -0500

    made some server file and index changes

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ server.listen(process.env.PORT || 3000);
 console.log('Server running...');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  connections.splice(connections.indexOf(socket), 1);
+  console.log('Disconnected: %s sockets connected', connections.length);
+});
