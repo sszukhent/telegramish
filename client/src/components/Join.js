783b1ab23431ccaf85f23b301ff734e864d2c0f4
sszukhent <commandershepard989@gmail.com>
2020-02-20 18:54:48 -0500

    initial client and server setup

---
+++ client/src/components/Join.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return (
+    <div>
+      <h1>Join</h1>
+    </div>
+  );
+};
+
+export default Join;
