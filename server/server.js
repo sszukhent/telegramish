a70c59b7d99b2a824ee8ba6374aa49e9282ef541
sszukhent <commandershepard989@gmail.com>
2020-03-03 17:14:22 -0500

    setup new routes and models

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ const io = socketio(server);
 // Connect to DB
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 io.on('connection', socket => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
